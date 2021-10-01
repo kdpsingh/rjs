7b6d40c75252968f1218f0df0483e9317c39871f
Karandeep Singh <kdpsingh@umich.edu>
2021-10-01 04:26:04 -0400

    Support upload of .RData files using a file html attribute

--- r.js
+++ r.js
@@ -1,13 +1,18 @@
 rjs = function () {
   // find all the HTML elements with a class of 'r-code'
-  r_code_blocks = document.querySelectorAll('.r-code');
+  r_code_blocks = document.querySelectorAll('.r-code')
+  
   
   r_code_blocks.forEach(function (code_block) {
-    let xhttp_initial_request = new XMLHttpRequest();
     
+    let code_text = code_block.innerHTML
+    
+    code_block.innerHTML = '&block;&block;&block;&block;&block;&blk12;&blk12;&blk12;&blk12;&blk12;'
+    
+    let xhttp_initial_request = new XMLHttpRequest()
     xhttp_initial_request.onreadystatechange = function() {
       if (this.readyState == 4) {
-        if (this.status == 201) {  
+        if (this.status == 201) { 
           let output_url = this.responseText.match(/tmp\/(.*?output.\w+)/);          
           output_url = 'http://tmp.ocpu.io/' + output_url[1];          
           console.log(output_url);          
@@ -35,11 +40,37 @@ rjs = function () {
         code_block.classList.add("r-code-output");
       }
     };
-    
     xhttp_initial_request.open('POST', 'http://kdpsingh.ocpu.io/RendereR/R/renderer', true);
-    xhttp_initial_request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8"');
-    xhttp_initial_request.send(JSON.stringify({"text": encodeURIComponent(code_block.innerHTML)}));
+    
+    var formData = new FormData();
+   
+    formData.append('text', JSON.stringify({"text": encodeURIComponent(code_text)}))
+  
+   if (code_block.hasAttribute('file')) {
+   let file_xhr = new XMLHttpRequest();
+
+    file_xhr.open("GET", code_block.getAttribute('file'), true);
+    file_xhr.responseType = 'blob'
+
+    file_xhr.onload = function(e)
+    {
+      if (file_xhr.readyState === 4)
+      {
+        
+        file = file_xhr.response;
+        
+        formData.append('file', file, 'file.RData')
+     xhttp_initial_request.send(formData)
+      }
+    }
+    file_xhr.send();
+  } else {
+    xhttp_initial_request.send(formData)
+  }
+    
+
   });
+  
 }
 
 window.onload = function () {
